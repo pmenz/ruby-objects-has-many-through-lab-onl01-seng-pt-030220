21aa5ae15c2cdbe19860bc774e708e63fb2a9e1d
Pablo Menz <p.menz.r@gmail.com>
2020-04-16 01:55:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,26 @@
-class Artist 
-  
-  @@all= []
-  
-  def initiliaze(name, genre)
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
     @name = name
-    @genre = genre
-  end
-  
-  attr_accessor :name 
-  @name = name
-  @@all << self 
+    @@all << self
   end
 
-  def self.all 
-  @all 
+  def self.all
+    @@all
   end
-  
+
   def new_song(name, genre)
     Song.new(name, self, genre)
   end
-  
-  
-  
-  d
 
+  def songs
+    Song.all.select { |song| song.artist == self }
+  end
+
+  def genres
+    songs.map(&:genre)
+  end
 end
